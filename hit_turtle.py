e96edab9155586be666cb21189d78d95b982e0ad
kjhgme <kjhgme@naver.com>
2021-09-13 15:02:52 +0900

    Update hit_turtle.py

--- hit_turtle.py
+++ hit_turtle.py
@@ -6,7 +6,11 @@ def drunken_move():
     turtle.forward(random.randint(50,300))
     turtle.stamp()
 
+def restart():
+    turtle.reset()
+
 turtle.shape('turtle')
 
 turtle.onkey(drunken_move, ' ')
+turtle.onkey(restart, 'Escape')
 turtle.listen()
