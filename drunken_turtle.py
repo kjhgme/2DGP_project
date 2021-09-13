28e8d08d813e0b5ea6db886980d9b2025ea87e2d
kjhgme <kjhgme@naver.com>
2021-09-13 14:01:54 +0900

    drunken_level

--- drunken_turtle.py
+++ drunken_turtle.py
@@ -3,6 +3,7 @@ import random
 
 turtle.shape('turtle')
 while (True):
-    turtle.setheading(random.randint(0,360))
+    drunken_level = random.randint(1, 10)
+    turtle.setheading(random.randint(0,36*drunken_level))
     turtle.forward(random.randint(100,200))
     turtle.stamp()
