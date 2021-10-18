86fcfad3948292f811e59546d600cd5cd39ba4d5
kjhgme <kjhgme@naver.com>
2021-10-18 15:14:13 +0900

    start, title, mygame, main_state

--- Lecture10_Game_Framework/main_state.py
+++ Lecture10_Game_Framework/main_state.py
@@ -46,12 +46,14 @@ class Boy:
 
 
 def enter():
-    pass
-
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
 
 def exit():
-    pass
-
+    global boy, grass
+    del(boy)
+    del(grass)
 
 def pause():
     pass
@@ -62,15 +64,25 @@ def resume():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
 
 
 def update():
-    pass
+    boy.update()
 
 
 def draw():
-    pass
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+
 
 
 
